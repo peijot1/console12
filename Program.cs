451060a3a28aa7c6869764b2bdb10918a2ae27d0
Piotr Jot <Piotr Jot>
2016-05-10 00:36:54 +0200

    Add alghoritm, and tests are working

--- Program.cs
+++ Program.cs
@@ -13,7 +13,28 @@ namespace ConsoleApplication12
         {
         }
         public static void QuickSort(int[] array, int left, int right)
-        { }
+        {
+            int i = left;
+            int j = right;
+            int pivot = array[right];
+            while (i < j)
+            {
+                while (array[i] < pivot) i++;
+                while (array[j] > pivot) j--;
+
+                if (i <= j)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+
+            }
+            if (left < j) QuickSort(array, left, j);
+            if (i < right) QuickSort(array, i, right);
+        }
     }
 
 
